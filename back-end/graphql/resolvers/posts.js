ca10ff0e200b2a9a3cdd0816f5fd9a953a046fa7
iagpomponet <iagosilbernagel@hotmail.com>
2020-12-05 20:32:18 -0300

    feat: createPost resolver

--- back-end/graphql/resolvers/posts.js
+++ back-end/graphql/resolvers/posts.js
@@ -1,16 +1,29 @@
 const Post = require('../../src/models/Post');
 
 module.exports = {
-  Query: {
-    async getPosts(){
-      try {
-        const posts = await Post.find();
+	Query: {
+		async getPosts() {
+			try {
+				const posts = await Post.find();
 
-        return posts
-      }
-      catch(err){
-        throw new Error(err);
-      }
-    }
-  }
-}
+				return posts;
+			} catch (err) {
+				throw new Error(err);
+			}
+		},
+	},
+	Mutations: {
+		async createPost(_, { postInput: { body, username } }) {
+			const token = jwt.verify(token);
+
+			const newPost = new Post({
+				body,
+				username,
+			});
+
+			await newPost.save();
+
+			return newPost;
+		},
+	},
+};
