9626f0c6c0701be86123c2e47beb4b64bdf6747c
iagpomponet <iagosilbernagel@hotmail.com>
2020-12-05 20:32:39 -0300

    perf: user validation before register

--- back-end/graphql/resolvers/users.js
+++ back-end/graphql/resolvers/users.js
@@ -1,46 +1,71 @@
 const User = require('../../src/models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
+const { validateRegisterInput } = require('../../src/utils/validators');
 
 module.exports = {
-  Mutation: {
-    register : async (_ , 
-      {
-        registerInput: { username, password, confirmPassword, email }
-      }
-      ) => {
-           
-      password = await bcrypt.hash(password, 12);
+	Mutation: {
+		register: async (
+			_,
+			{ registerInput: { username, password, confirmPassword, email } },
+		) => {
+			console.log('validateRegisterInput :>> ', validateRegisterInput);
+			const { errors, valid } = validateRegisterInput(
+				username,
+				email,
+				password,
+				confirmPassword,
+			);
 
-      const newUser = new User({
-        email, 
-        username,
-        password,
-        createdAt: new Date().toISOString()
-      });
+			if (!valid) {
+				console.log('validateRegisterInput :>> ', validateRegisterInput);
+				console.log('errors :>> ', errors, valid);
+				throw new UserInputError('Errors', { errors });
+			}
 
-      const res = await newUser.save();
+			const user = await User.findOne({ email });
 
-      const token = jwt.sign({
-        id: res.id,
-        email: res.email,
-        username: res.username
-      }, 'secret_key');
+			if (user) {
+				throw new UserInputError('E-mail address already taken', {
+					errors: {
+						email: 'E-mail already taken',
+					},
+				});
+			}
 
-      return newUser;
-    }
-  },
-  
-  Query: {
-    users: async () => {
-      try {
-        const users = await User.find()
-        return users;
-      }
-      
-      catch(err){
-        throw new Error(err);
-      }
-    }
-  }
-}
+			password = await bcrypt.hash(password, 12);
+
+			const newUser = new User({
+				email,
+				username,
+				password,
+				createdAt: new Date().toISOString(),
+			});
+
+			const res = await newUser.save();
+
+			const token = jwt.sign(
+				{
+					id: res.id,
+					email: res.email,
+					username: res.username,
+				},
+				'secret_key',
+			);
+
+			return newUser;
+		},
+	},
+
+	Query: {
+		users: async () => {
+			try {
+				const users = await User.find();
+				return users;
+			} catch (err) {
+				throw new Error(err);
+			}
+		},
+	},
+};
